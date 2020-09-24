1030d688d0a7c7ef6d8ca3ca0b9b93051afc00e4
DanielaAcevedo <71802621+DanielaAcevedo@users.noreply.github.com>
2020-09-24 18:30:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Hola, Ingresa los siguienetes datos, para realizar tu registro");
+            Console.Write("Ingrese su edad:  ");
+            a = int.Parse(Console.ReadLine());
+            if (a >= 18 )
+            {
+                Console.WriteLine("Bienvenido  tu registro ha sido Exitoso!");
+
+            }
+            else
+            {
+                Console.WriteLine("Lo sentimos por ser menor de edad no puedes continuar");
+            }
+
+        }
+    }
+}
